fc4b74d9336d3c8c963a58df859d25655bf48dfc
bin.shen <bin.shen@openx.com>
2016-06-04 11:26:13 +0800

    updates

--- server.js
+++ server.js
@@ -3,21 +3,16 @@
  */
 
 var net = require('net');
-
-
 var mongoClient = require('mongodb').MongoClient;
+var config = require('./config');
 
-var HOST = '121.40.92.176';
-var PORT = 6789;
-var URL = 'mongodb://127.0.0.1:27017/moral_db';
-var COLLECTION = 'documents';
-
-mongoClient.connect(URL, function(err, db) {
+mongoClient.connect(config.URL, function(err, db) {
     if (err) return;
-    console.log('Connecting to Mongo DB at ' + URL);
+    console.log('Connecting to Mongo DB at ' + config.URL);
 
     net.createServer(function(sock) {
         console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
+
         sock.on('data', function(data) {
             if(data == null) {
                 console.log("error - invalid data - null or undefined");
@@ -40,31 +35,32 @@ mongoClient.connect(URL, function(err, db) {
 
             //2.云地址写入WIFI模块
             if(value.startsWith('5a0033010002')) {
-                sock.write(new Buffer([ 0x6A, 0x00, 0x51, 0x01, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA2, 0x2A, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x02, 0x00, 0x04, 0x07, 0x10, 0x03, 0x17, 0x10, 0x10, 0x00, 0x01, 0x01, 0x1A, 0x85, 0x79, 0x28, 0x5C, 0xB0, 0x00, 0x05, 0x5C, 0xB0, 0x00, 0x05, 0x5C, 0xB0, 0x00, 0x00, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xF3, 0x6B ]));
+                sock.write(new Buffer(config.OUTPUT_2));
                 return;
             }
 
             //6.配网数据
             if(value.startsWith('5a0033010006')) {
-                sock.write(new Buffer([ 0x6A, 0x00, 0x0A, 0x01, 0x00, 0x0C, 0xF2, 0x2F, 0x38, 0x6B ]));
+                sock.write(new Buffer(config.OUTPUT_6));
                 return;
             }
 
             //12.联云通知
             if(value.startsWith('5a000a01000c')) {
-                sock.write(new Buffer([ 0x6A, 0x00, 0x0A, 0x01, 0x00, 0x0C, 0xF2, 0x2F, 0x38, 0x6B ]));
+                sock.write(new Buffer(config.OUTPUT_6));
                 return;
             }
         });
         sock.on('close', function(data) {
             console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
         });
-    }).listen(PORT, HOST);
-    console.log('TCP Server listening on ' + HOST +':'+ PORT);
+    }).listen(config.PORT, config.HOST);
+    
+    console.log('TCP Server listening on ' + config.HOST +':'+ config.PORT);
 });
 
 var insertDocument = function(db, data, callback) {
-    var collection = db.collection(COLLECTION);
+    var collection = db.collection(config.COLLECTION);
     collection.insertOne({ data: data, date: Date.now() }, function(err, result) {
         if (err) return;
         callback(result);
--- test.js
+++ test.js
@@ -1,68 +1,98 @@
-/**
- * Created by bin.shen on 5/30/16.
- */
+// /**
+//  * Created by bin.shen on 5/30/16.
+//  */
+//
+// var str = "5a002f010001";
+// var key1 = "5a002f010001a1b245";
+// var key2 = "5a002f010101a1b245";
+// console.log(key1.startsWith(str));
+// console.log(key2.startsWith(str));
+//
+// var data = "a1B2c3";
+// console.log(data.toString("hex").toLowerCase());
+//
+// function rand_string(n) {
+//     if (n <= 0) {
+//         return '';
+//     }
+//     var rs = '';
+//     try {
+//         rs = crypto.randomBytes(Math.ceil(n/2)).toString('hex').slice(0,n);
+//         /* note: could do this non-blocking, but still might fail */
+//     }
+//     catch(ex) {
+//         /* known exception cause: depletion of entropy info for randomBytes */
+//         console.error('Exception generating random string: ' + ex);
+//         /* weaker random fallback */
+//         rs = '';
+//         var r = n % 8, q = (n-r)/8, i;
+//         for(i = 0; i < q; i++) {
+//             rs += Math.random().toString(16).slice(2);
+//         }
+//         if(r > 0){
+//             rs += Math.random().toString(16).slice(2,i);
+//         }
+//     }
+//     return rs;
+// }
+//
+// console.log(rand_string(64))
 
-var net = require('net');
+// require('crypto').randomBytes(32, function(err, buffer) {
+//     var hex = buffer.toString('hex');
+//     console.log(hex.length)
+//     console.log(hex)
+// });console.log(hex)
 
+// var crypto = require('crypto');
+// var hex = crypto.randomBytes(32).toString('hex');
+// console.log(hex.length);
+// console.log(hex);
 
-var mongoClient = require('mongodb').MongoClient;
+//faae3e645e9cb3cfbb91f9e46eabefeacf42799af82218ec886c4bf04f9fef29
 
-var HOST = '121.40.92.176';
-var PORT = 6789;
-var URL = 'mongodb://127.0.0.1:27017/moral_db';
-var COLLECTION = 'documents';
+// var crypto = require('crypto');
+// var cipher = crypto.createCipher('aes-256-cbc','faae3e645e9cb3cfbb91f9e46eabefeacf42799af82218ec886c4bf04f9fef29')
+// var text = "5a000a010002f00f0c5b";
+// var crypted = cipher.update(text,'utf8','hex');
+// crypted += cipher.final('hex');
+// console.log(crypted);
+//
+// var decipher = crypto.createDecipher('aes-256-cbc','faae3e645e9cb3cfbb91f9e46eabefeacf42799af82218ec886c4bf04f9fef29')
+// var dec = decipher.update(crypted,'hex','utf8');
+// dec += decipher.final('utf8');
+// console.log(dec);
 
+// var mongoClient = require('mongodb').MongoClient;
+// var URL = 'mongodb://121.40.92.176:27017/moral_db';
+// var COLLECTION = 'data';
+//
+// mongoClient.connect(URL, function(err, db) {
+//     if (err) return;
+//     console.log('Connecting to Mongo DB at ' + URL);
+//
+//     insertDocument(db, {'aaa':'2222'}, function(data) {
+//         console.log('++++++++')
+//     });
+// });
+//
+// var insertDocument = function(db, data, callback) {
+//     var collection = db.collection(COLLECTION);
+//     collection.insertOne({ data: data, date: Date.now() }, function(err, result) {
+//         if (err) return;
+//         callback(result);
+//     });
+// };
 
-var insertDocument = function(db, data, callback) {
-    var collection = db.collection(COLLECTION);
-    /*
-     var length = data.length;
-     if(length != 64) return;
-     var fields = data.match(/.{2}/g);
-     var items = {};
-     for(var i in fields) {
-     items['f' + (parseInt(i) + 1)] = fields[i];
-     }
-     collection.insertMany([ items ], function(err, result) {
-     if (err) return;
-     callback(result);
-     });
-     */
-    collection.insertMany([ { data: data, date: Date.now() } ], function(err, result) {
-        if (err) return;
-        callback(result);
-    });
-};
+// data = "5a003301000300000000000000000000000000000000000000000000000000000000006c01d60000c90000140000000000575b";
+// var fields = data.match(/.{2}/g);
+// var items = {};
+// for(var i in fields) {
+//     items[i] = fields[i];
+// }
+//
+// console.log(items);
+// console.log(items[50]);
 
-mongoClient.connect(URL, function(err, db) {
-    if (err) return;
-    console.log('Connecting to Mongo DB at ' + URL);
-
-    net.createServer(function(sock) {
-        console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-        sock.on('data', function(data) {
-            var value = data.toString('hex');
-            insertDocument(db, value, function(result) {
-                //sock.write(JSON.stringify(result));
-            });
-
-            if(value === '5a000a010002f00f0c5b') {
-                sock.write(new Buffer([ 0x6A, 0x00, 0x0A, 0x01, 0x00, 0x2, 0xF0, 0x0F, 0x0C, 0x6B ]));
-            } else if(value === '5a000a010004f22f305b') {
-                sock.write(new Buffer([ 0x6A, 0x00, 0x0A, 0x01, 0x00, 0x04, 0xF2, 0x2F, 0x30, 0x6B ]));
-            } else {
-                //sock.write("1\n");
-            }
-            /*
-             if(value === '5a000a010004f22f305b') {
-             sock.write(new Buffer([ 0x6A, 0x00, 0x0A, 0x01, 0x00, 0x04, 0xF2, 0x2F, 0x30, 0x6B ]));
-             }
-             */
-        });
-
-        sock.on('close', function(data) {
-            console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-        });
-    }).listen(PORT, HOST);
-    console.log('TCP Server listening on ' + HOST +':'+ PORT);
-});
+var config = require('./config');
+console.log(config.HOST);
